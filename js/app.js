15d222050949386781c1bac757ea12138af41baf
juanmllanos <juan_m@hotmail.com.ar>
2023-04-14 20:29:41 -0300

    mejora de la logica, se hizo responsive, se agrego el alert de bootstrap

--- js/app.js
+++ js/app.js
@@ -2,28 +2,64 @@ const $botonComenzar = document.getElementById('comenzarJuego');
 const $areaInput = document.getElementById('areaInput');
 const $botonConfirmarNumero = document.getElementById('botonOk');
 const $numeroUsuario = document.getElementById('numero');
+const $espacioRespuesta = document.getElementById('espacioRespuesta');
 
-let numeroParaAdivinar = null;
-let contadorIntentos = 0;
-
-function compararNumeros(numero) {
-	if (numero === numeroParaAdivinar) {
-		alert(`Adivinaste el numero! (${contadorIntentos} intentos)`);
-		$areaInput.className = 'd-none';	
-	} else if (numero > numeroParaAdivinar) {
-		alert(`El numero es menor que ${numero}`);
-	} else alert(`El numero es mayor que ${numero}`);
+function borrarRespuestas() {
+	const $respuestas = document.querySelectorAll('.respuesta');
+	$respuestas.forEach(respuesta => respuesta.remove());
 };
 
-$botonComenzar.onclick = function() {
-	if (!numeroParaAdivinar) numeroParaAdivinar = Math.ceil(Math.random() * 100)
+function crearRespuesta() {
+	const $respuesta = document.createElement('div');
+	$respuesta.className = 'alert alert-info respuesta';
+	$respuesta.role = 'alert';
+	$espacioRespuesta.appendChild($respuesta);
+};
+
+function textoRespuesta(numero) {
+	const $textoRespuesta = document.querySelector('.respuesta');
+
+	if (numero === numeroParaAdivinar) {
+		$textoRespuesta.innerText = `Adivinaste el numero! Era el ${numeroParaAdivinar}. (${contadorIntentos} intentos)`;
+		$areaInput.className = 'd-none';
+		nuevoJugo = true;
+	};
+	if (numero > numeroParaAdivinar) {
+		$textoRespuesta.innerText = `El numero es menor que ${numero}`;
+	};
+	if (numero < numeroParaAdivinar) {
+		$textoRespuesta.innerText = `El numero es mayor que ${numero}`;
+	};
+};
+
+function compararNumeros(numero) {
+	if (numero > 99) {
+		alert('El numero tiene que ser de dos digitos!')
+	} else {
+		borrarRespuestas();
+		crearRespuesta(numero);
+		textoRespuesta(numero);
+	};
+};
+
+let numeroParaAdivinar = null;
+let nuevoJugo = true;
+let contadorIntentos = 0;
+
+$botonComenzar.onclick = function () {
+	borrarRespuestas();
+	contadorIntentos = 0;
+	if (!numeroParaAdivinar || nuevoJugo === true) {
+		numeroParaAdivinar = Math.floor(Math.random() * 100);
+		nuevoJugo = false;
+	};
 	$areaInput.className = '';
 };
 
-$botonConfirmarNumero.onclick = function(event) {
+$botonConfirmarNumero.addEventListener('click', (event) => {
 	const numeroElegido = Number($numeroUsuario.value);
-	contadorIntentos ++;
+	contadorIntentos++;
 	compararNumeros(numeroElegido);
 	$numeroUsuario.value = '';
 	event.preventDefault();
-};
+});
